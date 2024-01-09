e9dca520dbdb0b195199eac26c4ae596a8805213
Aryaman Singh <aryaman.singh@rooton.ca>
2024-01-09 13:28:21 +0530

    Added Email Auth API

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ import re
 import openai
 import time
 from cachetools import TTLCache, cached
+import math as m
+import random as r
+from email.message import EmailMessage
+import ssl
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import datetime
 
 load_dotenv()
 cache = TTLCache(maxsize=1000000, ttl=86400)
@@ -26,6 +34,10 @@ class CourseRequest(BaseModel):
 
 class CourseResponse(BaseModel):
     data: dict
+
+class EmailRequest(BaseModel):
+    email: str
+
 
 # Function to fetch all data (similar to your script)
 @cached(cache)
@@ -137,7 +149,6 @@ def cleandict(my_dict):
             del my_dict[key]
     return my_dict
 
-
 def core_model(fos, level,college_df):
     if (level != "false" or fos != "false"):
 
@@ -184,8 +195,6 @@ def core_model(fos, level,college_df):
         return df
     else:
         return {"Error": "Please Select Atleast Single Option"}
-
-
 
 @app.post("/recommend_courses", response_model=CourseResponse)
 async def recommend_courses(request: CourseRequest):
@@ -345,6 +354,119 @@ async def recommend_courses(request: CourseRequest):
     
     return CourseResponse(data={"message": "Course recommendations generated successfully","recommended_courses": recommendData,
                                 "response_time": response_time})
+
+def email_verification(receiver_emailID):
+    email_sender=os.getenv("EMAIL")
+    email_password=os.getenv("EMAIL_PSK")
+    email_receiver=receiver_emailID
+    subject="Email Confirmation"
+    string = '0123456789'
+    OTP = "" 
+    varlen= len(string) 
+    for i in range(6) : 
+        OTP += string[m.floor(r.random() * varlen)] 
+
+    Body="""
+        Welcome to Root-on!
+        THis is verfication code {0}
+    """.format(OTP)
+
+    em=EmailMessage()
+    msgRoot = MIMEMultipart('related')
+    msgRoot['Subject'] = 'Confirm Your Email Address'
+    msgRoot['From'] = email_sender
+    msgRoot['To'] = email_receiver
+    msgRoot.preamble = 'This is a multi-part message in MIME format.'
+    msgAlternative = MIMEMultipart('alternative')
+    msgRoot.attach(msgAlternative)
+
+    msgText = MIMEText('This is the alternative plain text message.')
+    msgAlternative.attach(msgText)
+
+    msgText = MIMEText("""
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+  </head>
+  <div id="__react-email-preview" style="display:none;overflow:hidden;line-height:1px;opacity:0;max-height:0;max-width:0">Your Account Verification Code Is ***-***<div> ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿ ‌‍‎‏﻿</div>
+  </div>
+  <table style="width:100%;background-color:#ffffff;margin:0 auto;font-family:-apple-system, BlinkMacSystemFont, &#x27;Segoe UI&#x27;, &#x27;Roboto&#x27;, &#x27;Oxygen&#x27;, &#x27;Ubuntu&#x27;, &#x27;Cantarell&#x27;, &#x27;Fira Sans&#x27;, &#x27;Droid Sans&#x27;, &#x27;Helvetica Neue&#x27;, sans-serif" align="center" border="0" cellPadding="0" cellSpacing="0" role="presentation">
+    <tbody>
+      <tr>
+        <td>
+          <div><!--[if mso | IE]>
+            <table role="presentation" width="100%" align="center" style="max-width:600px;margin:0 auto;"><tr><td></td><td style="width:37.5em;background:#ffffff">
+          <![endif]--></div>
+          <div style="max-width:600px;margin:0 auto">
+            <table style="width:100%;margin-top:32px" align="center" border="0" cellPadding="0" cellSpacing="0" role="presentation">
+              <tbody>
+                <tr>
+                  <td><img alt="Rooton" src="https://i.postimg.cc/wMn5hJ9g/rooton.png" width="auto" height="50" style="display:block;outline:none;border:none;text-decoration:none" /></td>
+                </tr>
+              </tbody>
+            </table>
+            <h1 style="color:#1d1c1d;font-size:36px;font-weight:700;margin:30px 0;padding:0;line-height:42px">Confirm your email address</h1>
+            <p style="font-size:20px;line-height:28px;margin:16px 0;margin-bottom:30px">Your confirmation code is below - enter it in your open browser window and we&
+            <table style="width:100%;background:rgb(245, 244, 245);border-radius:4px;margin-right:50px;margin-bottom:30px;padding:43px 23px" align="center" border="0" cellPadding="0" cellSpacing="0" role="presentation">
+              <tbody>
+                <tr>
+                  <td>
+                    <p style="font-size:30px;line-height:24px;margin:16px 0;text-align:center;vertical-align:middle">{0}</p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <p style="font-size:14px;line-height:24px;margin:16px 0;color:#000">If you didn&
+            <table style="margin-bottom:32px;width:100%" border="0" cellPadding="0" cellSpacing="10" align="left">
+              <tr>
+                <td align="left" valign="top"><img alt="Rooton" src="https://i.postimg.cc/wMn5hJ9g/rooton.png" width="auto" height="50" style="display:block;outline:none;border:none;text-decoration:none" /></td>
+                <td align="right" valign="top"><a target="_blank" style="color:#067df7;text-decoration:none" href="https://www.facebook.com/pg/rooton/"><img alt="Rooton" src="https://cdn-icons-png.flaticon.com/512/739/739237.png" width="32" height="32" style="display:inline;outline:none;border:none;text-decoration:none;margin-left:32px" /></a><a target="_blank" style="color:#067df7;text-decoration:none" href="https://instagram.com/rootonofficial"><img alt="Slack" src="https://cdn-icons-png.flaticon.com/512/87/87390.png" width="32" height="32" style="display:inline;outline:none;border:none;text-decoration:none;margin-left:32px" /></a><a target="_blank" style="color:#067df7;text-decoration:none" href="https://www.linkedin.com/in/ronak-patel-rcic/"><img alt="Rooton" src="https://cdn-icons-png.flaticon.com/512/220/220343.png" width="32" height="32" style="display:inline;outline:none;border:none;text-decoration:none;margin-left:32px" /></a></td>
+              </tr>
+            </table>
+            <table style="width:100%;font-size:12px;color:#b7b7b7;line-height:15px;text-align:left;margin-bottom:50px" align="center" border="0" cellPadding="0" cellSpacing="0" role="presentation">
+              <tbody>
+                <tr>
+                  <td><a target="_blank" style="color:#b7b7b7;text-decoration:underline" href="https://rooton.ca" rel="noopener noreferrer">Our blog</a>   |   <a target="_blank" style="color:#b7b7b7;text-decoration:underline" href="https://slack.com/legal" rel="noopener noreferrer">Policies</a>   |   <a target="_blank" style="color:#b7b7b7;text-decoration:underline" href="https://slack.com/help" rel="noopener noreferrer">Help center</a>   |   <a target="_blank" style="color:#b7b7b7;text-decoration:underline" href="https://slack.com/community" rel="noopener noreferrer" data-auth="NotApplicable" data-linkindex="6">Slack Community</a>
+                    <p style="font-size:12px;line-height:15px;margin:16px 0;color:#b7b7b7;text-align:left;margin-bottom:50px">©2022 Rooton Technologies, LLC, a Salesforce company. <br />500 Howard Street, San Francisco, CA 94105, USA <br /><br />All rights reserved.</p>
+                    <p style="font-size:12px;line-height:15px;margin:16px 0;color:#b7b7b7;text-align:left;margin-bottom:50px">©2022 Rooton Technologies, LLC, a Salesforce company. <br />500 Howard Street, San Francisco, CA 94105, USA <br /><br />A{1}</p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div><!--[if mso | IE]>
+          </td><td></td></tr></table>
+          <![endif]--></div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</html>
+""".format(OTP,datetime.datetime.now()), 'html')
+    msgAlternative.attach(msgText)
+
+    em['From']=email_sender
+    em['To']=email_receiver
+    em['Subject']=subject
+    em.set_content(Body)
+
+    context=ssl.create_default_context()
+
+    with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
+        smtp.login(email_sender,email_password)
+        smtp.sendmail(email_sender, email_receiver, msgRoot.as_string())
+
+    return OTP
+
+@app.post("/send-otp")
+def send_otp(request: EmailRequest):
+    try:
+        # Call your email_verification function here
+        otp = email_verification(request.email)
+        return {"OTP": otp}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Error sending email")
 
 if __name__ == "__main__":
     import uvicorn
